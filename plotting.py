a27f12b1c0830bad82473c66a4af67ba357500e9
Andrii Magalich <andrew.magalich@gmail.com>
2014-09-05 19:05:39 +0200

    Rethink the plotting method

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 import numpy
+import itertools
 import matplotlib.pyplot as plt
 import numericalunits as nu
 
@@ -10,30 +11,40 @@ class Plotting:
 
     def __init__(self):
         self.params_figure, self.plots = plt.subplots(2, 2, num=1)
+        self.plots = [p for p in itertools.chain(*self.plots)]
         self.params_figure.subplots_adjust(hspace=0.5, wspace=0.5)
 
-        self.plots[0][0].set_title("Temperature")
-        self.plots[0][0].set_xlabel("time, s")
-        self.plots[0][0].set_ylabel("T, MeV")
-        self.plots[0][0].set_yscale("log")
+        self.plot_map = ['T', 'a', 'aT', 'rho']
+        self.divider_map = [UNITS.MeV, 1, UNITS.MeV, UNITS.eV**4]
 
-        self.plots[0][1].set_title("Scale factor")
-        self.plots[0][1].set_xlabel("time, s")
-        self.plots[0][1].set_ylabel("a, 1")
+        self.plots[0].set_title("Temperature")
+        self.plots[0].set_xlabel("time, s")
+        self.plots[0].set_ylabel("T, MeV")
+        self.plots[0].set_yscale("log")
+        self.plots[0].set_ylim(0, 10)
 
-        self.plots[1][0].set_title("T * a")
-        self.plots[1][0].set_xlabel("time, s")
-        self.plots[1][0].set_ylabel("T * a, MeV")
+        self.plots[1].set_title("Scale factor")
+        self.plots[1].set_xlabel("time, s")
+        self.plots[1].set_ylabel("a, 1")
+        self.plots[0].set_ylim(1, 10)
 
-        self.plots[1][1].set_title("Total energy density")
-        self.plots[1][1].set_yscale("log")
-        self.plots[1][1].set_xlabel("time, s")
-        self.plots[1][1].set_ylabel("rho, MeV**4")
+        self.plots[2].set_title("T * a")
+        self.plots[2].set_xlabel("time, s")
+        self.plots[2].set_ylabel("T * a, MeV")
+        self.plots[0].set_ylim(1, 10)
 
-        self.line00, = self.plots[0][0].plot([1], [1], 'b.')
-        self.line01, = self.plots[0][1].plot([1], [1], 'b.')
-        self.line10, = self.plots[1][0].plot([1], [1], 'b.')
-        self.line11, = self.plots[1][1].plot([1], [1], 'b.')
+        self.plots[3].set_title("Total energy density")
+        self.plots[3].set_yscale("log")
+        self.plots[3].set_xlabel("time, s")
+        self.plots[3].set_ylabel("rho, eV**4")
+        self.plots[0].set_ylim(1, 10)
+
+        self.lines = []
+        self.plots_data = []
+        self.times = ring_deque([], 1000)
+        for plot in self.plots:
+            self.lines.append(plot.plot([], [], 'b-')[0])
+            self.plots_data.append(ring_deque([], 1000))
 
         self.params_figure.show()
 
@@ -55,33 +66,26 @@ class Plotting:
             self.particles_figure.show()
 
     def plot(self, data, full=False):
-        if not full:
-            ts = data['t'][-1]
-            Ts = data['T'][-1]
-            As = data['a'][-1]
-            rhos = data['rho'][-1]
-            style = 'b.'
-        else:
-            ts = numpy.array(data['t'])
-            Ts = numpy.array(data['T'])
-            As = numpy.array(data['a'])
-            rhos = numpy.array(data['rho'])
-            style = 'b-'
 
-        self.line00.set_xdata(ts / UNITS.s)
-        self.line00.set_ydata(Ts / nu.MeV)
-        self.line01.set_xdata(ts / UNITS.s)
-        self.line01.set_ydata(As)
-        self.line10.set_xdata(ts / UNITS.s)
-        self.line10.set_ydata(Ts * As / nu.MeV)
-        self.line11.set_xdata(ts / UNITS.s)
-        self.line11.set_ydata(rhos / nu.MeV**4)
-        # line00, = self.plots[0][0].plot(ts / UNITS.s, Ts / nu.MeV, style, animated=True)
-        # line01, = self.plots[0][1].plot(ts / UNITS.s, As, style, animated=True)
-        # line10, = self.plots[1][0].plot(ts / UNITS.s, Ts * As / nu.MeV, style, animated=True)
-        # line11, = self.plots[1][1].plot(ts / UNITS.s, rhos / nu.MeV**4, style, animated=True)
+        last_t = data['t'][-1] / UNITS.s
+        self.times.append(last_t)
 
-        # print(line00, line01, line10, line11)
+        for i, plot in enumerate(self.plots):
+            xmin, xmax = plot.get_xlim()
+            ymin, ymax = plot.get_ylim()
+
+            if last_t >= xmax:
+                plot.set_xlim(self.times[0], last_t * 1.5)
+
+            last_data = data[self.plot_map[i]][-1] / self.divider_map[i]
+            self.plots_data[i].append(last_data)
+
+            if last_data >= ymax:
+                plot.set_ylim(self.plots_data[i].min, 1.5 * last_data)
+            if last_data <= ymin:
+                plot.set_ylim(last_data / 1.5, self.plots_data[i].max)
+
+            self.lines[i].set_data(self.times, self.plots_data[i])
 
         plt.figure(1)
         plt.draw()
@@ -89,13 +93,13 @@ class Plotting:
         if self.particles:
             for i, particle in enumerate(self.particles):
                 if not particle.in_equilibrium:
-                    self.particles_plots[i*2].plot(GRID.TEMPLATE / nu.MeV, particle._distribution)
+                    self.particles_plots[i*2].plot(GRID.TEMPLATE / UNITS.MeV, particle._distribution)
                     feq = particle.distribution_function_vectorized(
                         particle.energy_normalized_vectorized(GRID.TEMPLATE)
                         / particle.temperature
                     )
                     self.particles_plots[i*2 + 1].plot(
-                        GRID.TEMPLATE / nu.MeV,
+                        GRID.TEMPLATE / UNITS.MeV,
                         particle._distribution / feq
                     )
 
