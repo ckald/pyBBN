15569333bcfd6f06fb643a30503dfbbf35be412f
Andrii Magalich <andrew.magalich@gmail.com>
2014-09-11 18:56:22 +0200

    Interpolate collision integral too

--- particles/NonEqParticle.py
+++ particles/NonEqParticle.py
@@ -2,7 +2,8 @@
 General non-equilibrium particles
 """
 import numpy
-from common import lambda_integrate, PARAMS, momentum_to_index
+from scipy import interpolate
+from common import lambda_integrate, PARAMS, momentum_to_index, GRID
 
 
 name = 'non-equilibrium'
@@ -18,9 +19,16 @@ def density(particle):
 
 @lambda_integrate
 def energy_density(particle):
-    return numpy.vectorize(lambda p: (
-        particle.distribution(p) * p ** 2
-        * particle.energy(p)
+    """ == Energy density ==
+
+        \begin{equation}
+            \rho = \frac{g}{2 \pi^2} \frac{m^4}{x^4} \int dy y^2 \sqrt{y^2 +\
+            \frac{M_N^2 x^2}{m^2}} f(y)
+        \end{equation}
+    """
+    return numpy.vectorize(lambda y: (
+        particle.distribution(y) * y ** 2
+        * particle.energy_normalized(y)
         * particle.dof / 2. / numpy.pi**2 / PARAMS.a**4
     ), otypes=[numpy.float_])
 
@@ -36,11 +44,14 @@ def pressure(particle):
 
 @lambda_integrate
 def numerator(particle):
-    return numpy.vectorize(lambda y: (
+    integral = interpolate.interp1d(GRID.TEMPLATE, particle.collision_integral,
+                                    kind='quadratic', assume_sorted=True, copy=False)
+    func = numpy.vectorize(lambda y: (
         -1. * particle.dof / 2. / numpy.pi**2
         * y**2 * particle.energy_normalized(y)
-        * particle.collision_integral[momentum_to_index(y)]
+        * integral(y)
     ), otypes=[numpy.float_])
+    return func
 
 
 def denominator(particle):
