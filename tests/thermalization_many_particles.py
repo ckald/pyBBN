1e1b7778cfb85b6766b87de3788589209a854a45
Andrii Magalich <andrew.magalich@gmail.com>
2014-11-02 15:53:20 +0100

    Use the particle library

--- tests/thermalization_many_particles.py
+++ tests/thermalization_many_particles.py
@@ -1,5 +1,5 @@
 from interaction import Interaction
-from particles import Particle, STATISTICS
+from particles import Particle, library
 from evolution import Universe
 from common import CONST, UNITS, PARAMS, GRID
 
@@ -12,75 +12,98 @@ PARAMS.infer()
 
 Particles = []
 Interactions = []
-photon = Particle(name='Photon',
-                  statistics=STATISTICS.BOSON)
-Particles.append(photon)
 
-neutrino_e = Particle(name='Electron neutrino',
-                      statistics=STATISTICS.FERMION,
-                      dof=4,
-                      decoupling_temperature=3 * UNITS.MeV)
-Particles.append(neutrino_e)
+photon = Particle(**library.StandardModelParticles.photon)
+electron = Particle(**library.StandardModelParticles.electron)
+neutrino_e = Particle(**library.StandardModelParticles.neutrino_e)
+neutrino_mu = Particle(**library.StandardModelParticles.neutrino_mu)
+neutrino_tau = Particle(**library.StandardModelParticles.neutrino_tau)
 
-neutrino_mu = Particle(name='Muon neutrino',
-                       statistics=STATISTICS.FERMION,
-                       dof=4,
-                       decoupling_temperature=3 * UNITS.MeV)
-Particles.append(neutrino_mu)
+Particles += [photon, electron, neutrino_e, neutrino_mu, neutrino_tau]
 
-
+""" \begin{align}
+        \nu_e + \nu_e &\to \nu_e + \nu_e
+        \\\\ \nu_e + \overline{\nu_e} &\to \nu_e + \overline{\nu_e}
+    \end{align} """
 neutrino_e_scattering = Interaction(
     in_particles=[neutrino_e, neutrino_e],
     out_particles=[neutrino_e, neutrino_e],
     decoupling_temperature=0 * UNITS.MeV,
-    K1=128 * CONST.G_F**2,
+    K1=(64 + 128) * CONST.G_F**2,
     K2=0.,
-    order=(0, 1, 2, 3),
-    symmetry_factor=0.5
+    order=(0, 1, 2, 3)
 )
 Interactions.append(neutrino_e_scattering)
 
+""" \begin{align}
+        \nu_\mu + \nu_\mu &\to \nu_\mu + \nu_\mu
+        \\\\ \nu_\mu + \overline{\nu_\mu} &\to \nu_\mu + \overline{\nu_\mu}
+    \end{align} """
 neutrino_mu_scattering = Interaction(
     in_particles=[neutrino_mu, neutrino_mu],
     out_particles=[neutrino_mu, neutrino_mu],
     decoupling_temperature=0 * UNITS.MeV,
-    K1=128 * CONST.G_F**2,
+    K1=(64 + 128) * CONST.G_F**2,
     K2=0.,
-    order=(0, 1, 2, 3),
-    symmetry_factor=0.5
+    order=(0, 1, 2, 3)
 )
 Interactions.append(neutrino_mu_scattering)
 
+neutrino_tau_scattering = Interaction(
+    in_particles=[neutrino_tau, neutrino_tau],
+    out_particles=[neutrino_tau, neutrino_tau],
+    decoupling_temperature=0 * UNITS.MeV,
+    K1=(64 + 128) * CONST.G_F**2,
+    K2=0.,
+    order=(0, 1, 2, 3)
+)
+Interactions.append(neutrino_tau_scattering)
+
+""" \begin{align}
+        \nu_e + \nu_\mu &\to \nu_e + \nu_\mu
+        \\\\ \nu_e + \overline{\nu_\mu} &\to \nu_e + \overline{\nu_\mu}
+    \end{align} """
 neutrino_e_mu_scattering = Interaction(
     in_particles=[neutrino_e, neutrino_mu],
     out_particles=[neutrino_e, neutrino_mu],
     decoupling_temperature=0 * UNITS.MeV,
-    K1=128 * CONST.G_F**2,
+    K1=32 * CONST.G_F**2 * 2,
     K2=0.,
-    order=(0, 1, 2, 3),
-    symmetry_factor=1.
+    order=(0, 1, 2, 3)
 )
 Interactions.append(neutrino_e_mu_scattering)
 
-neutrino_mu_e_scattering = Interaction(
-    in_particles=[neutrino_mu, neutrino_e],
-    out_particles=[neutrino_e, neutrino_mu],
+neutrino_mu_tau_scattering = Interaction(
+    in_particles=[neutrino_tau, neutrino_mu],
+    out_particles=[neutrino_tau, neutrino_mu],
     decoupling_temperature=0 * UNITS.MeV,
-    K1=128 * CONST.G_F**2,
+    K1=32 * CONST.G_F**2 * 2,
     K2=0.,
-    order=(0, 1, 2, 3),
-    symmetry_factor=1.
+    order=(0, 1, 2, 3)
 )
-Interactions.append(neutrino_mu_e_scattering)
+Interactions.append(neutrino_mu_tau_scattering)
 
+neutrino_tau_e_scattering = Interaction(
+    in_particles=[neutrino_tau, neutrino_e],
+    out_particles=[neutrino_tau, neutrino_e],
+    decoupling_temperature=0 * UNITS.MeV,
+    K1=32 * CONST.G_F**2 * 2,
+    K2=0.,
+    order=(0, 1, 2, 3)
+)
+Interactions.append(neutrino_tau_e_scattering)
+
+""" Add non-equilibrium parts to the distribution functions """
 import numpy
 neutrino_e._distribution += \
     numpy.vectorize(lambda x: 0.01 * numpy.exp(-(x/UNITS.MeV-5)**2))(GRID.TEMPLATE)
 neutrino_mu._distribution += \
-    numpy.vectorize(lambda x: 0.01 * numpy.exp(-(x/UNITS.MeV-2.5)**2))(GRID.TEMPLATE)
+    numpy.vectorize(lambda x: 0.01 * numpy.exp(-(x/UNITS.MeV-5)**2))(GRID.TEMPLATE)
+neutrino_tau._distribution += \
+    numpy.vectorize(lambda x: 0.01 * numpy.exp(-(x/UNITS.MeV-5)**2))(GRID.TEMPLATE)
 
 universe = Universe(Particles, Interactions)
-universe.graphics.monitor(particles=[neutrino_e, neutrino_mu])
+universe.graphics.monitor(particles=[neutrino_e, neutrino_mu, neutrino_tau])
 universe.evolve()
 
 raw_input("...")
