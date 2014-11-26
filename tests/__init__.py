0b2609b6ca4b61b39d7dfb908778cbb6c4f87ad9
Andrii Magalich <andrew.magalich@gmail.com>
2014-11-26 01:02:39 +0100

    Update tests index

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,5 @@
 """
-== Functioning simple tests: ==
+== Functioning tests: ==
 
   * [Photon universe](photon_universe/index.html) (perfect)
     :   radiation equilibrium physics and expansion
@@ -9,16 +9,13 @@
     :   check that electron-positron annihilation influences on the system entropy
   * [Cosmic neutrino background temperature](cosmic_neutrino_temperature/index.html) (good)
     :   check that additional particles do not spoil the temperature ratio `1.401`
-
-== Planned simple tests: ==
-
-  * Test expansion of the free non-equilibrium species
   * Thermalization test (equilibrium distribution with a "bump")
+  * [Standard Model BBN](standard_model_bbn/index.html) (good)
+    :   verify the Standard Model neutrino decoupling
+
+== Planned tests: ==
+
   * Neutron decay test
-  * Neutrino self-scattering test
-
-== Planned complex tests: ==
-
   * Check previous results for single sterile neutrino species
   * Test two-body decays
 """
