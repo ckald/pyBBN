f138aa37859f8c0505fc7d4f685a8b992cf9258e
Andrii Magalich <andrew.magalich@gmail.com>
2014-12-09 14:46:09 +0100

    Vectorize remap_interval internally

--- common/integrators.py
+++ common/integrators.py
@@ -99,15 +99,15 @@ def remap_interval(f, x, y, bounds):
     add_x = (b + a) / 2.
     norm_x = sub_x * x + add_x
 
-    h_x = h(norm_x)
-    g_x = g(norm_x)
+    h_x = numpy.vectorize(h)(norm_x)
+    g_x = numpy.vectorize(g)(norm_x)
     sub_y = (h_x - g_x) / 2.
     add_y = (h_x + g_x) / 2.
     norm_y = sub_y * y + add_y
 
     return sub_x * sub_y * f(norm_x, norm_y)
 
-remap_interval = numpy.vectorize(remap_interval, otypes=[numpy.float_], excluded=['f', 'bounds'])
+# remap_interval = numpy.vectorize(remap_interval, otypes=[numpy.float_], excluded=['f', 'bounds'])
 
 
 def double_gaussian(f, a, b, g, h):
