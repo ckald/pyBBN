cec9b5a293759b67d903347dd788a2a6d74b7f53
Andrii Magalich <andrew.magalich@gmail.com>
2015-05-11 03:26:33 +0200

    Clean up a bit

--- common/parallelization.py
+++ common/parallelization.py
@@ -1,6 +1,5 @@
 import multiprocessing
-from multiprocessing import Process, Pipe, Pool
-from itertools import izip
+from multiprocessing import Pool
 
 
 def target(args, **kw):
@@ -11,31 +10,6 @@ def target(args, **kw):
 
 worker_count = multiprocessing.cpu_count() / 2
 pool = Pool(processes=worker_count)
-
-
-def spawn(f):
-    def fun(pipe, x):
-        pipe.send(f(x))
-        pipe.close()
-    return fun
-
-
-def parmap(f, X):
-    pipe = [Pipe() for x in X]
-    processes = [Process(target=spawn(f), args=(c, x)) for x, (_, c) in izip(X, pipe)]
-    numProcesses = len(processes)
-    processNum = 0
-    outputList = []
-    while processNum < numProcesses:
-        endProcessNum = min(processNum+worker_count, numProcesses)
-        for proc in processes[processNum:endProcessNum]:
-            proc.start()
-        for proc in processes[processNum:endProcessNum]:
-            proc.join()
-        for proc, c in pipe[processNum:endProcessNum]:
-            outputList.append(proc.recv())
-        processNum = endProcessNum
-    return outputList
 
 
 def poolmap(cls, func_name, arguments):
