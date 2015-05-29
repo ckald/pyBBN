d4b503c7d2d0dafd0463384c722a50f09e496f81
Andrii Magalich <andrew.magalich@gmail.com>
2015-05-29 20:43:37 +0200

    Fix decorator

--- common/utils.py
+++ common/utils.py
@@ -129,7 +129,7 @@ def trace_unhandled_exceptions(func):
     @functools.wraps(func)
     def wrapped_func(*args, **kwargs):
         try:
-            func(*args, **kwargs)
+            return func(*args, **kwargs)
         except:
             print 'Exception in '+func.__name__
             traceback.print_exc()
