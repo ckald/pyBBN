ba0164176de2db3d23ef933c3a052b2d7ed12bea
Andrii Magalich <andrew.magalich@gmail.com>
2015-06-18 13:34:07 +0200

    Change naming

--- tests/paper_oscillations/__main__.py
+++ tests/paper_oscillations/__main__.py
@@ -37,7 +37,7 @@ lifetime = float(args.tau) * UNITS.s
 
 folder = utils.ensure_dir(
     os.path.split(__file__)[0],
-    "mass={:e}_theta={:e}_tau={:e}".format(mass / UNITS.MeV, theta, lifetime / UNITS.s)
+    "mass={:e}_tau={:e}_theta={:e}".format(mass / UNITS.MeV, lifetime / UNITS.s, theta)
     + args.comment
 )
 
