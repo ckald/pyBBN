b8275e745a52ef5b523c913fcaf38e82c1dde245
Andrii Magalich <andrew.magalich@gmail.com>
2015-06-19 21:34:15 +0200

    Add another parameter to the LogSpacedGrid initialization

--- common/__init__.py
+++ common/__init__.py
@@ -163,12 +163,12 @@ class LogSpacedGrid(object):
 
     __slots__ = ('MIN_MOMENTUM', 'MAX_MOMENTUM', 'BOUNDS', 'MOMENTUM_SAMPLES', 'TEMPLATE')
 
-    def __init__(self, MAX_MOMENTUM=20 * UNITS.MeV):
+    def __init__(self, MOMENTUM_SAMPLES=50, MAX_MOMENTUM=20 * UNITS.MeV):
         # self.MIN_MOMENTUM = 1. * UNITS.eV
         self.MIN_MOMENTUM = 0
         self.MAX_MOMENTUM = self.MIN_MOMENTUM + MAX_MOMENTUM
         self.BOUNDS = (self.MIN_MOMENTUM, self.MAX_MOMENTUM)
-        self.MOMENTUM_SAMPLES = 50
+        self.MOMENTUM_SAMPLES = MOMENTUM_SAMPLES
 
         """
         Grid template can be copied when defining a new distribution function and is convenient to\
