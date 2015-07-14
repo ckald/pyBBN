bdc16fdca8df4ec81c9e7fb68cf49f48482fa101
Andrii Magalich <andrew.magalich@gmail.com>
2015-07-15 02:14:50 +0300

    Update ancient tests

--- tests/photon_electron_universe/__main__.py
+++ tests/photon_electron_universe/__main__.py
@@ -20,24 +20,25 @@ from library.SM import particles as SMP
 from common import Params, UNITS
 
 
+T_final = 100 * UNITS.keV
 params = Params(T=100 * UNITS.MeV,
-                dx=1e-1 * UNITS.MeV)
+                dy=0.05)
 
-universe = Universe(params=params, logfile="tests/photon_electron_universe/log.txt")
+universe = Universe(params=params, folder="tests/photon_electron_universe")
 
 photon = Particle(**SMP.photon)
 electron = Particle(**SMP.leptons.electron)
 
 universe.add_particles([photon, electron])
-universe.evolve()
+universe.evolve(T_final)
 
 initial_aT = universe.data['aT'][0]
 print "a * T is not conserved: {}"\
     .format(any([initial_aT != value for value in universe.data['aT']]))
 initial_a = universe.data['a'][len(universe.data['a'])/2]
 initial_t = universe.data['t'][len(universe.data['a'])/2] / UNITS.s
-last_a = universe.data['a'][-1]
-last_t = universe.data['t'][-1] / UNITS.s
+last_a = universe.data['a'].iloc[-1]
+last_t = universe.data['t'].iloc[-1] / UNITS.s
 
 print """
     a scaling discrepancy is: {:.2f}%"""\
--- tests/photon_universe/__main__.py
+++ tests/photon_universe/__main__.py
@@ -22,21 +22,21 @@ from common import Params, UNITS
 
 T_final = 100 * UNITS.keV
 params = Params(T=100 * UNITS.MeV,
-                dx=1e-2 * UNITS.MeV)
+                dy=0.05)
 
-universe = Universe(params=params, logfile="tests/photon_universe/log.txt")
+universe = Universe(params=params, folder="tests/photon_universe")
 
 photon = Particle(**SMP.photon)
 
-universe.add_particles(photon)
+universe.add_particles([photon])
 universe.evolve(T_final)
 
 initial_aT = universe.data['aT'][0]
 print "a * T is conserved: {}".format(all([initial_aT == value for value in universe.data['aT']]))
 initial_a = universe.data['a'][len(universe.data['a'])/2]
 initial_t = universe.data['t'][len(universe.data['a'])/2] / UNITS.s
-last_a = universe.data['a'][-1]
-last_t = universe.data['t'][-1] / UNITS.s
+last_a = universe.data['a'].iloc[-1]
+last_t = universe.data['t'].iloc[-1] / UNITS.s
 
 print "a scaling discrepancy is: {:.2f}%"\
     .format(100 * (last_a / initial_a / numpy.sqrt(last_t / initial_t) - 1))
