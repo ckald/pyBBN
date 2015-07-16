da7205e87cf095129989d8b1354bcba22e3560d3
Andrii Magalich <andrew.magalich@gmail.com>
2015-07-16 19:19:51 +0300

    Crazy

--- tests/paper_oscillations/__main__.py
+++ tests/paper_oscillations/__main__.py
@@ -110,9 +110,9 @@ if universe.graphics:
     ])
 
 universe.evolve(T_kawano, export=False)
-universe.params.dy = 0.025
+universe.params.dy = 0.003125
 universe.evolve(T_interactions_freeze_out, export=False)
-universe.params.dy = 0.0125
+universe.params.dy = 0.003125
 universe.interactions = tuple()
 universe.evolve(T_final)
 
