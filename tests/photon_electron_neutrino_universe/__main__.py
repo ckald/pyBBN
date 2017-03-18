9ff80b39959a6846daae0567288929c0e71ff41b
Anastasia Filimonova <filimonovastasya@gmail.com>
2017-03-18 20:16:23 +0200

    test photo-electron-neutrino universe is added but not changed

---
+++ tests/photon_electron_neutrino_universe/__main__.py
@@ -0,0 +1,58 @@
+"""
+## Photon-electron universe test
+
+<img src="plots.svg" width=100% />
+
+This test checks that in the photon-electron universe:
+
+  * $a \propto t^{1/2}$
+  * $a * T$ is not conserved by a factor around `1.401`
+
+[Log file](log.txt)
+
+"""
+
+import numpy
+
+from particles import Particle
+from evolution import Universe
+from library.SM import particles as SMP
+from common import Params, UNITS
+
+
+T_final = 20 * UNITS.keV
+params = Params(T=100 * UNITS.MeV,
+                dy=0.003)
+
+universe = Universe(params=params, folder="tests/photon_electron_neutrino_universe")
+
+photon = Particle(**SMP.photon)
+electron = Particle(**SMP.leptons.electron)
+neutrino_e = Particle(**SMP.leptons.neutrino_e)
+neutrino_mu = Particle(**SMP.leptons.neutrino_mu)
+neutrino_tau = Particle(**SMP.leptons.neutrino_tau)
+
+universe.add_particles([photon, electron, neutrino_e, neutrino_mu, neutrino_tau])
+universe.evolve(T_final)
+
+initial_aT = universe.data['aT'][0]
+print "a * T is not conserved: {}"\
+    .format(any([initial_aT != value for value in universe.data['aT']]))
+initial_a = universe.data['a'][len(universe.data['a'])/2]
+initial_t = universe.data['t'][len(universe.data['a'])/2] / UNITS.s
+last_a = universe.data['a'].iloc[-1]
+last_t = universe.data['t'].iloc[-1] / UNITS.s
+
+print """
+    a scaling discrepancy is: {:.2f}%"""\
+    .format(100 * (last_a / initial_a - numpy.sqrt(last_t / initial_t))
+            / numpy.sqrt(last_t / initial_t))
+
+print """
+    Cosmic photon background temperature is {:.3f} times bigger than cosmic neutrinos temperature.
+    Relative error is {:.3f} %""".format(universe.params.aT / UNITS.MeV,
+                                         (universe.params.aT / UNITS.MeV - 1.401) / 1.401 * 100)
+
+universe.graphics.save(__file__)
+
+raw_input("...")
