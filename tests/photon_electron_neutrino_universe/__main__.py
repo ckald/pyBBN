93d921361ecd706180263fddbd9a6ae63a3ae413
Anastasia Filimonova <filimonovastasya@gmail.com>
2017-03-18 20:58:27 +0200

    test changed to new format

--- tests/photon_electron_neutrino_universe/__main__.py
+++ tests/photon_electron_neutrino_universe/__main__.py
@@ -12,6 +12,7 @@ This test checks that in the photon-electron universe:
 
 """
 
+import os
 import numpy
 
 from particles import Particle
@@ -19,12 +20,13 @@ from evolution import Universe
 from library.SM import particles as SMP
 from common import Params, UNITS
 
+folder = os.path.join(os.path.split(__file__)[0], 'output')
 
 T_final = 20 * UNITS.keV
 params = Params(T=100 * UNITS.MeV,
                 dy=0.003)
 
-universe = Universe(params=params, folder="tests/photon_electron_neutrino_universe")
+universe = Universe(params=params, folder=folder)
 
 photon = Particle(**SMP.photon)
 electron = Particle(**SMP.leptons.electron)
@@ -38,10 +40,10 @@ universe.evolve(T_final)
 initial_aT = universe.data['aT'][0]
 print "a * T is not conserved: {}"\
     .format(any([initial_aT != value for value in universe.data['aT']]))
-initial_a = universe.data['a'][len(universe.data['a'])/2]
-initial_t = universe.data['t'][len(universe.data['a'])/2] / UNITS.s
-last_a = universe.data['a'].iloc[-1]
-last_t = universe.data['t'].iloc[-1] / UNITS.s
+initial_a = universe.data['a'][len(universe.data)/2]
+initial_t = universe.data['t'][len(universe.data)/2] / UNITS.s
+last_a = universe.data['a'][-1]
+last_t = universe.data['t'][-1] / UNITS.s
 
 print """
     a scaling discrepancy is: {:.2f}%"""\
@@ -53,6 +55,3 @@ print """
     Relative error is {:.3f} %""".format(universe.params.aT / UNITS.MeV,
                                          (universe.params.aT / UNITS.MeV - 1.401) / 1.401 * 100)
 
-universe.graphics.save(__file__)
-
-raw_input("...")
