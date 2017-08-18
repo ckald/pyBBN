2585406a58631fcd2653e8191b2bab30df2ea4ed
Andrii Magalich <andrew.magalich@gmail.com>
2017-08-18 18:10:05 +0300

    Make doubles long

--- interactions/four_particle/cpp/integral.cpp
+++ interactions/four_particle/cpp/integral.cpp
@@ -1,7 +1,7 @@
 #include <pybind11/pybind11.h>
 #include <pybind11/stl.h>
 #include <iostream>
-#include <math.h>
+#include <cmath>
 #include <array>
 #include <vector>
 
@@ -10,29 +10,29 @@ using namespace pybind11::literals;
 
 
 struct M_t {
-    M_t(std::array<int, 4> order, double K1, double K2)
+    M_t(std::array<int, 4> order, long double K1, long double K2)
         : order(order), K1(K1), K2(K2) {}
     std::array<int, 4> order;
-    double K1;
-    double K2;
+    long double K1;
+    long double K2;
 };
 
 struct grid_t {
-    grid_t(std::vector<double> grid, std::vector<double> distribution, int size)
+    grid_t(std::vector<long double> grid, std::vector<long double> distribution, int size)
         : grid(grid), distribution(distribution), size(size) {}
-    std::vector<double> grid;
-    std::vector<double> distribution;
+    std::vector<long double> grid;
+    std::vector<long double> distribution;
     int size;
 };
 
 struct particle_t {
-    particle_t(int eta, double m, grid_t grid, int in_equilibrium, double aT)
+    particle_t(int eta, long double m, grid_t grid, int in_equilibrium, long double aT)
         : eta(eta), m(m), grid(grid), in_equilibrium(in_equilibrium), aT(aT) {}
     int eta;
-    double m;
+    long double m;
     grid_t grid;
     int in_equilibrium;
-    double aT;
+    long double aT;
 };
 
 struct reaction_t {
@@ -41,12 +41,12 @@ struct reaction_t {
     int side;
 };
 
-double energy(double y, double mass=0) {
+long double energy(long double y, long double mass=0) {
     return sqrt(y*y + mass*mass);
 }
 
 
-int binary_find(const std::vector<double> &grid, unsigned int size, double x) {
+int binary_find(const std::vector<long double> &grid, unsigned int size, long double x) {
     unsigned int head = 0, tail = size - 1;
     unsigned int middle;
 
@@ -68,11 +68,11 @@ int binary_find(const std::vector<double> &grid, unsigned int size, double x) {
 }
 
 
-double distribution_interpolation(const std::vector<double> &grid,
-                                  const std::vector<double> &distribution,
-                                  double p, double m=0, int eta=1) {
+long double distribution_interpolation(const std::vector<long double> &grid,
+                                       const std::vector<long double> &distribution,
+                                       long double p, long double m=0, int eta=1) {
 
-    double p_low(-1), p_high(-1);
+    long double p_low(-1), p_high(-1);
     unsigned int i(0), i_low(0), i_high(0);
 
     i = binary_find(grid, grid.size(), p);
@@ -88,7 +88,7 @@ double distribution_interpolation(const std::vector<double> &grid,
     i_high = i + 1;
     p_high = grid[i_high];
 
-    double E_p, E_low, E_high, g_high, g_low, g;
+    long double E_p, E_low, E_high, g_high, g_low, g;
 
     // === Exponential interpolation ===
     E_p = energy(p, m);
@@ -105,32 +105,32 @@ double distribution_interpolation(const std::vector<double> &grid,
     g_high = distribution[i_high];
     g_low = distribution[i_low];
 
-    g_high = (1. / g_high - eta);
+    g_high = (1.L / g_high - eta);
     if (g_high > 0) {
         g_high = log(g_high);
     } else {
-        return 0.;
+        return 0.L;
     }
 
-    g_low = (1. / g_low - eta);
+    g_low = (1.L / g_low - eta);
     if (g_low > 0) {
         g_low = log(g_low);
     } else {
-        return 0.;
+        return 0.L;
     }
 
     g = ((E_p - E_low) * g_high + (E_high - E_p) * g_low) / (E_high - E_low);
 
-    g = 1. / (exp(g) + eta);
+    g = 1.L / (exp(g) + eta);
     if (isnan(g)) {
-        return 0.;
+        return 0.L;
     }
     return g;
 
 }
 
 
-double D1(double q1, double q2, double q3, double q4) {
+long double D1(long double q1, long double q2, long double q3, long double q4) {
     /* Dimensionality: energy
 
         \begin{align}
@@ -143,23 +143,23 @@ double D1(double q1, double q2, double q3, double q4) {
     if (q3 < q4) { std::swap(q3, q4); }
 
     if ((q1 > q2 + q3 + q4) || (q3 > q2 + q1 + q4)) {
-        return 0.;
+        return 0.L;
     }
 
     if (q1 + q2 >= q3 + q4) {
         if (q1 + q4 >= q2 + q3) {
-            return 0.5 * (-q1 + q2 + q3 + q4);
+            return 0.5L * (-q1 + q2 + q3 + q4);
         }
         return q4;
     }
 
     if (q1 + q4 < q2 + q3)  {
-        return 0.5 * (q1 + q2 - q3 + q4);
+        return 0.5L * (q1 + q2 - q3 + q4);
     }
     return q2;
 }
 
-double D2(double q1, double q2, double q3, double q4) {
+long double D2(long double q1, long double q2, long double q3, long double q4) {
     /* Dimensionality: pow(energy, 3)
 
         \begin{align}
@@ -175,34 +175,34 @@ double D2(double q1, double q2, double q3, double q4) {
     if (q3 < q4) { std::swap(q3, q4); }
 
     if ((q1 > q2 + q3 + q4) || (q3 > q2 + q1 + q4)) {
-        return 0.;
+        return 0.L;
     }
 
     if (q1 + q2 >= q3 + q4) {
         if (q1 + q4 >= q2 + q3) {
-            double a = q1 - q2;
+            long double a = q1 - q2;
             return (
-                a * (pow(a, 2) - 3. * (pow(q3, 2) + pow(q4, 2))) + 2. * (pow(q3, 3) + pow(q4, 3))
-            ) / 12.;
+                a * (pow(a, 2) - 3.L * (pow(q3, 2) + pow(q4, 2))) + 2.L * (pow(q3, 3) + pow(q4, 3))
+            ) / 12.L;
         }
         else {
-            return pow(q4, 3) / 3.;
+            return pow(q4, 3) / 3.L;
         }
     } else {
         if (q1 + q4 >= q2 + q3) {
-            return q2 * (3. * (pow(q3, 2) + pow(q4, 2) - pow(q1, 2)) - pow(q2, 2)) / 6.;
+            return q2 * (3.L * (pow(q3, 2) + pow(q4, 2) - pow(q1, 2)) - pow(q2, 2)) / 6.L;
         }
         else {
-            double a = q1 + q2;
+            long double a = q1 + q2;
             return (
-                a * (3. * (pow(q3, 2) + pow(q4, 2)) - pow(a, 2)) + 2. * (pow(q4, 3) - pow(q3, 3))
-            ) / 12.;
+                a * (3.L * (pow(q3, 2) + pow(q4, 2)) - pow(a, 2)) + 2.L * (pow(q4, 3) - pow(q3, 3))
+            ) / 12.L;
         }
     }
 }
 
 
-double D3(double q1, double q2, double q3, double q4) {
+long double D3(long double q1, long double q2, long double q3, long double q4) {
     /* Dimensionality: pow(energy, 5)
 
         \begin{align}
@@ -219,43 +219,43 @@ double D3(double q1, double q2, double q3, double q4) {
     if (q3 < q4) { std::swap(q3, q4); }
 
     if ((q1 > q2 + q3 + q4) || (q3 > q2 + q1 + q4)) {
-        return 0.;
+        return 0.L;
     }
 
     if (q1 + q2 >= q3 + q4) {
         if (q1 + q4 >= q2 + q3) {
             return (
                 pow(q1, 5) - pow(q2, 5) - pow(q3, 5) - pow(q4, 5)                                               // pow(E, 5)
-                + 5. * (
+                + 5.L * (
                     pow(q1, 2) * pow(q2, 2) * (q2 - q1)                                               // pow(E, 5)
                     + pow(q3, 2) * (pow(q2, 3) - pow(q1, 3) + (pow(q2, 2) + pow(q1, 2)) * q3)                        // pow(E, 5)
                     + pow(q4, 2) * (pow(q2, 3) - pow(q1, 3) + pow(q3, 3) + (pow(q1, 2) + pow(q2, 2) + pow(q3, 2)) * q4)        // pow(E, 5)
                 )
-            ) / 60.;
+            ) / 60.L;
         }
         else {
-            return pow(q4, 3) * (5. * (pow(q1, 2) + pow(q2, 2) + pow(q3, 2)) - pow(q4, 2)) / 30.;
+            return pow(q4, 3) * (5.L * (pow(q1, 2) + pow(q2, 2) + pow(q3, 2)) - pow(q4, 2)) / 30.L;
         }
     } else {
         if (q1 + q4 >= q2 + q3) {
-            return pow(q2, 3) * (5. * (pow(q1, 2) + pow(q3, 2) + pow(q4, 2)) - pow(q2, 2)) / 30.;
+            return pow(q2, 3) * (5.L * (pow(q1, 2) + pow(q3, 2) + pow(q4, 2)) - pow(q2, 2)) / 30.L;
         }
         else {
             return (
                 pow(q3, 5) - pow(q4, 5) - pow(q1, 5) - pow(q2, 5)
-                + 5. * (
+                + 5.L * (
                     pow(q3, 2) * pow(q4, 2) * (q4 - q3)
                     + pow(q1, 2) * (pow(q4, 3) - pow(q3, 3) + (pow(q4, 2) + pow(q3, 2)) * q1)
                     + pow(q2, 2) * (pow(q4, 3) - pow(q3, 3) + pow(q1, 3) + (pow(q1, 2) + pow(q3, 2) + pow(q4, 2)) * q2)
                 )
-            ) / 60.;
+            ) / 60.L;
         }
     }
 }
 
 
-double D(const std::array<double, 4> &p, const std::array<double, 4> &E, const std::array<double, 4> &m,
-         double K1, double K2,
+long double D(const std::array<long double, 4> &p, const std::array<long double, 4> &E, const std::array<long double, 4> &m,
+         long double K1, long double K2,
          const std::array<int, 4> &order, const std::array<int, 4> &sides) {
     /* Dimensionality: energy */
 
@@ -268,16 +268,16 @@ double D(const std::array<double, 4> &p, const std::array<double, 4> &E, const s
     sksl = sides[k] * sides[l];
     sisjsksl = sides[i] * sides[j] * sides[k] * sides[l];
 
-    double result = 0.;
+    long double result = 0.L;
 
-    if (K1 != 0.) {
+    if (K1 != 0.L) {
         result += K1 * (E[0]*E[1]*E[2]*E[3] * D1(p[0], p[1], p[2], p[3]) + sisjsksl * D3(p[0], p[1], p[2], p[3]));
 
         result += K1 * (E[i]*E[j] * sksl * D2(p[i], p[j], p[k], p[l])
                         + E[k]*E[l] * sisj * D2(p[k], p[l], p[i], p[j]));
     }
 
-    if (K2 != 0.) {
+    if (K2 != 0.L) {
         result += K2 * m[i]*m[j] * (E[k]*E[l] * D1(p[0], p[1], p[2], p[3]) + sksl * D2(p[i], p[j], p[k], p[l]));
     }
 
@@ -285,23 +285,23 @@ double D(const std::array<double, 4> &p, const std::array<double, 4> &E, const s
 }
 
 
-double Db1(double q2, double q3, double q4) {
+long double Db1(long double q2, long double q3, long double q4) {
     if ((q2 + q3 > q4) && (q2 + q4 > q3) && (q3 + q4 > q2)) {
-        return 1.;
+        return 1.L;
     }
-    return 0.;
+    return 0.L;
 }
 
 
-double Db2(double q2, double q3, double q4) {
+long double Db2(long double q2, long double q3, long double q4) {
     if ((q2 + q3 > q4) && (q2 + q4 > q3) && (q3 + q4 > q2)) {
-        return 0.5 * (pow(q3, 2) + pow(q4, 2) - pow(q2, 2));
+        return 0.5L * (pow(q3, 2) + pow(q4, 2) - pow(q2, 2));
     }
-    return 0.;
+    return 0.L;
 }
 
-double Db(const std::array<double, 4> &p, const std::array<double, 4> &E, const std::array<double, 4> &m,
-          double K1, double K2,
+long double Db(const std::array<long double, 4> &p, const std::array<long double, 4> &E, const std::array<long double, 4> &m,
+          long double K1, long double K2,
           const std::array<int, 4> &order, const std::array<int, 4> &sides) {
     /* Dimensionality: energy */
 
@@ -314,28 +314,28 @@ double Db(const std::array<double, 4> &p, const std::array<double, 4> &E, const 
     sisj = sides[i] * sides[j];
     sksl = sides[k] * sides[l];
 
-    double result(0.), subresult(0.);
+    long double result(0.L), subresult(0.L);
 
-    if (K1 != 0.) {
+    if (K1 != 0.L) {
         subresult = E[1]*E[2]*E[3] * Db1(p[1], p[2], p[3]);
 
-        if (i * j == 0.) {
+        if (i * j == 0.L) {
             subresult += sisj * E[i+j] * Db2(p[i+j], p[k], p[l]);
         }
-        else if (k * l == 0.) {
+        else if (k * l == 0.L) {
             subresult += sksl * E[k+l] * Db2(p[i], p[j], p[k+l]);
         }
 
         result += K1 * subresult;
     }
 
-    if (K2 != 0.) {
-        subresult = 0.;
+    if (K2 != 0.L) {
+        subresult = 0.L;
 
-        if (i * j == 0.) {
+        if (i * j == 0.L) {
             subresult += m[i+j] * (E[k] * E[l] * Db1(p[1], p[2], p[3]) + sksl * Db2(p[i+j], p[k], p[l]));
         }
-        else if (k * l == 0.) {
+        else if (k * l == 0.L) {
             subresult += m[i] * m[j] * m[k+l] * Db1(p[1], p[2], p[3]);
         }
 
@@ -357,7 +357,7 @@ double Db(const std::array<double, 4> &p, const std::array<double, 4> &E, const 
     \end{align}
 */
 
-double F_A(const std::vector<reaction_t> &reaction, const std::array<double, 4> &f, int skip_index=-1) {
+long double F_A(const std::vector<reaction_t> &reaction, const std::array<long double, 4> &f, int skip_index=-1) {
     /*
     Forward reaction distribution functional term
 
@@ -368,7 +368,7 @@ double F_A(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
     :param skip_index: Particle to skip in the expression
     */
 
-    double temp(-1.);
+    long double temp(-1.L);
 
     for (int i = 0; i < 4; ++i) {
         if (i != skip_index) {
@@ -376,7 +376,7 @@ double F_A(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
             if (reaction[i].side == -1) {
                 temp *= f[i];
             } else {
-                temp *= 1. - reaction[i].specie.eta * f[i];
+                temp *= 1.L - reaction[i].specie.eta * f[i];
             }
         }
     }
@@ -384,7 +384,7 @@ double F_A(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
     return temp;
 }
 
-double F_B(const std::vector<reaction_t> &reaction, const std::array<double, 4> &f, int skip_index=-1) {
+long double F_B(const std::vector<reaction_t> &reaction, const std::array<long double, 4> &f, int skip_index=-1) {
     /*
     Backward reaction distribution functional term
 
@@ -395,7 +395,7 @@ double F_B(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
     :param skip_index: Particle to skip in the expression
     */
 
-    double temp(1.);
+    long double temp(1.L);
 
     for (int i = 0; i < 4; ++i) {
         if (i != skip_index) {
@@ -403,7 +403,7 @@ double F_B(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
             if (reaction[i].side == 1) {
                 temp *= f[i];
             } else {
-                temp *= 1. - reaction[i].specie.eta * f[i];
+                temp *= 1.L - reaction[i].specie.eta * f[i];
             }
         }
     }
@@ -430,24 +430,24 @@ double F_B(const std::vector<reaction_t> &reaction, const std::array<double, 4> 
 $^{(i)}$ in $\mathcal{F}^{(i)}$ means that the distribution function $f_i$ was omitted in the\
 corresponding expression. $\pm_j$ represents the $\eta$ value of the particle $j$.
 */
-double F_f(const std::vector<reaction_t> &reaction, const std::array<double, 4> &f) {
+long double F_f(const std::vector<reaction_t> &reaction, const std::array<long double, 4> &f) {
     /* Variable part of the distribution functional */
     return F_A(reaction, f, 0) - reaction[0].specie.eta * F_B(reaction, f, 0);
 }
 
-double F_1(const std::vector<reaction_t> &reaction, const std::array<double, 4> &f) {
+long double F_1(const std::vector<reaction_t> &reaction, const std::array<long double, 4> &f) {
     /* Constant part of the distribution functional */
     return F_B(reaction, f, 0);
 }
 
 
-double in_bounds(const std::array<double, 4> p, const std::array<double, 4> E, const std::array<double, 4> m) {
+long double in_bounds(const std::array<long double, 4> p, const std::array<long double, 4> E, const std::array<long double, 4> m) {
     /* $D$-functions involved in the interactions imply a cut-off region for the collision\
         integrand. In the general case of arbitrary particle masses, this is a set of \
         irrational inequalities that can hardly be solved (at least, Wolfram Mathematica does\
         not succeed in this). To avoid excessive computations, it is convenient to do an early\
         `return 0` when the particles kinematics lay out of the cut-off region */
-    double q1, q2, q3, q4;
+    long double q1, q2, q3, q4;
     q1 = p[0];
     q2 = p[1];
     q3 = p[2];
@@ -459,21 +459,21 @@ double in_bounds(const std::array<double, 4> p, const std::array<double, 4> E, c
     return (E[3] >= m[3] && q1 <= q2 + q3 + q4 && q3 <= q1 + q2 + q4);
 }
 
-std::pair<std::vector<double>, std::vector<double>> integrand(
-    double p0, const std::vector<double> &p1s, const std::vector<double> &p2s, int length,
+std::pair<std::vector<long double>, std::vector<long double>> integrand(
+    long double p0, const std::vector<long double> &p1s, const std::vector<long double> &p2s, int length,
     const std::vector<reaction_t> &reaction, const std::vector<M_t> &Ms
 ) {
     /*
     Collision integral interior.
     */
 
-    double ds, temp;
+    long double ds, temp;
 
-    std::array<double, 4> p, E, m;
+    std::array<long double, 4> p, E, m;
     std::array<int, 4> sides;
-    std::array<double, 4> f;
+    std::array<long double, 4> f;
 
-    std::vector<double> integrands_1(length, 0.), integrands_f(length, 0.);
+    std::vector<long double> integrands_1(length, 0.L), integrands_f(length, 0.L);
 
     for (int i = 0; i < 4; ++i) {
         sides[i] = reaction[i].side;
@@ -481,16 +481,16 @@ std::pair<std::vector<double>, std::vector<double>> integrand(
     }
 
     for (int i = 0; i < length; ++i) {
-        double p1 = p1s[i];
-        double p2 = p2s[i];
+        long double p1 = p1s[i];
+        long double p2 = p2s[i];
 
         if (p2 > p0 + p1) { continue; }
 
         p[0] = p0;
         p[1] = p1;
         p[2] = p2;
-        p[3] = 0.;
-        E[3] = 0.;
+        p[3] = 0.L;
+        E[3] = 0.L;
         for (int j = 0; j < 3; ++j) {
             E[j] = energy(p[j], m[j]);
             E[3] += sides[j] * E[j];
@@ -504,19 +504,19 @@ std::pair<std::vector<double>, std::vector<double>> integrand(
 
         if (!in_bounds(p, E, m)) { continue; }
 
-        temp = 1.;
+        temp = 1.L;
 
         // Avoid rounding errors and division by zero
         for (int k = 1; k < 3; ++k) {
-            if (m[k] != 0.) {
+            if (m[k] != 0.L) {
                 temp *= p[k] / E[k];
             }
         }
 
-        if (temp == 0.) { continue; }
+        if (temp == 0.L) { continue; }
 
-        ds = 0.;
-        if (p[0] != 0.) {
+        ds = 0.L;
+        if (p[0] != 0.L) {
             for (const M_t &M : Ms) {
                 ds += D(p, E, m, M.K1, M.K2, M.order, sides);
             }
@@ -528,13 +528,13 @@ std::pair<std::vector<double>, std::vector<double>> integrand(
         }
         temp *= ds;
 
-        if (temp == 0.) { continue; }
+        if (temp == 0.L) { continue; }
 
         // The distribution function of the main particle is not required here
         f[0] = -1;
         for (int k = 1; k < 4; ++k) {
             if (reaction[k].specie.in_equilibrium) {
-                f[k] = 1. / (
+                f[k] = 1.L / (
                     exp(energy(p[k], reaction[k].specie.m) / reaction[k].specie.aT)
                     + reaction[k].specie.eta
                 );
@@ -576,13 +576,13 @@ PYBIND11_MODULE(integral, m) {
           "reaction"_a, "Ms"_a);
 
     py::class_<M_t>(m, "M_t")
-        .def(py::init<std::array<int, 4>, double, double>(),
+        .def(py::init<std::array<int, 4>, long double, long double>(),
              "order"_a, "K1"_a, "K2"_a);
     py::class_<grid_t>(m, "grid_t")
-        .def(py::init<std::vector<double>, std::vector<double>, int>(),
+        .def(py::init<std::vector<long double>, std::vector<long double>, int>(),
              "grid"_a, "distribution"_a, "size"_a);
     py::class_<particle_t>(m, "particle_t")
-        .def(py::init<int, double, grid_t, int, double>(),
+        .def(py::init<int, long double, grid_t, int, long double>(),
              "eta"_a, "m"_a, "grid"_a, "in_equilibrium"_a, "aT"_a);
     py::class_<reaction_t>(m, "reaction_t")
         .def(py::init<particle_t, int>(),
