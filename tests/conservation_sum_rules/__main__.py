8061754a4a63d5796fe423965b910da12f50c739
Andrii Magalich <andrew.magalich@gmail.com>
2018-01-29 11:17:03 +0100

    Fixes and refactoring

--- tests/conservation_sum_rules/__main__.py
+++ tests/conservation_sum_rules/__main__.py
@@ -5,10 +5,7 @@ in the case of elastic processes only.
 
 import os
 import sys
-import numpy as np
 from collections import Counter
-
-sys.path.insert(0, "/".join(os.path.dirname(os.path.abspath(__file__)).split("/")[:-2]))
 
 from particles import Particle
 from library.SM import particles as SMP, interactions as SMI
@@ -16,47 +13,45 @@ from evolution import Universe
 from common import UNITS, Params
 from common import LinearSpacedGrid
 
+
 folder = os.path.join(os.path.split(__file__)[0], 'output')
 
-T_final = 0.03 * UNITS.MeV
+T_final = 0.1 * UNITS.MeV
 params = Params(T=10 * UNITS.MeV, dy=0.003125)
 
 universe = Universe(params=params, folder=folder)
 
-linear_grid = LinearSpacedGrid(MOMENTUM_SAMPLES=51, MAX_MOMENTUM=20*UNITS.MeV)
+linear_grid = LinearSpacedGrid(MOMENTUM_SAMPLES=201, MAX_MOMENTUM=20 * UNITS.MeV)
 
 photon = Particle(**SMP.photon)
 electron = Particle(**SMP.leptons.electron)
-neutrino_e = Particle(**SMP.leptons.neutrino_e, **{'grid': linear_grid})
-neutrino_mu = Particle(**SMP.leptons.neutrino_mu, **{'grid':linear_grid})
-neutrino_tau = Particle(**SMP.leptons.neutrino_tau,  **{'grid': linear_grid})
+neutrino_e = Particle(**SMP.leptons.neutrino_e, grid=linear_grid)
+neutrino_mu = Particle(**SMP.leptons.neutrino_mu, grid=linear_grid)
+neutrino_tau = Particle(**SMP.leptons.neutrino_tau, grid=linear_grid)
 
-neutrino_e.decoupling_temperature=9.5 * UNITS.MeV
-neutrino_mu.decoupling_temperature=9.5 * UNITS.MeV
-neutrino_tau.decoupling_temperature=9.5 * UNITS.MeV
+neutrino_e.decoupling_temperature = 10 * UNITS.MeV
+neutrino_mu.decoupling_temperature = 10 * UNITS.MeV
+neutrino_tau.decoupling_temperature = 10 * UNITS.MeV
 
 universe.add_particles([photon, electron, neutrino_e, neutrino_mu, neutrino_tau])
 
 interactions_pre = (
-	SMI.neutrino_interactions(leptons=[electron], neutrinos=[neutrino_e, neutrino_mu, neutrino_tau])
+    SMI.neutrino_interactions(leptons=[electron], neutrinos=[neutrino_e, neutrino_mu, neutrino_tau])
 )
 
 integrals = [[] for n in range(len(interactions_pre))]
 
 for num, interaction in enumerate(interactions_pre):
-    for integral in interaction.integrals:   
+    for integral in interaction.integrals:
         left_species = Counter(item.specie for item in integral.reaction if item.side == -1)
         right_species = Counter(item.specie for item in integral.reaction if item.side == 1)
-  
+
         if left_species == right_species:
             integrals[num].append(integral)
 
 for num, interaction in enumerate(interactions_pre):
-    if integrals[num] != []:
-        interaction.integrals = integrals[num]
-    else:    
-        interaction.integrals = [0]
-        
+    interaction.integrals = integrals[num]
+
 
 universe.interactions += interactions_pre
 
@@ -71,7 +66,7 @@ def step_monitor(universe):
                     f1.write('\t'.join(['{:e}'.format(x) for x in particle.grid.TEMPLATE / UNITS.MeV]) + '\n')
                 f1.write('\t'.join(['{:e}'.format(x) for x in particle.collision_integral]) + '\n')
             with open(os.path.join(folder, particle.name.replace(' ', '_') + ".number_and_energy_density.txt"), 'a') as f2:
-                f2.write('{:e}'.format(particle.density / UNITS.MeV**3) + '\t' + '{:e}'.format(universe.total_energy_density() / UNITS.MeV**4) + '\n')        
+                f2.write('{:e}'.format(particle.density / UNITS.MeV**3) + '\t' + '{:e}'.format(universe.total_energy_density() / UNITS.MeV**4) + '\n')
 
 universe.step_monitor = step_monitor
 
--- tests/conservation_sum_rules/test.py
+++ tests/conservation_sum_rules/test.py
@@ -1,20 +1,20 @@
 import os
 import numpy as np
-import sys
-
-#sys.path.insert(0, '/home/nashwan/Master Project/pyBBN')
-#from common import UNITS, CONST
 
 from matplotlib import pyplot as plt
-from scipy.integrate import simps, cumtrapz
+from scipy.integrate import simps
+
 
 folder = os.path.join(os.path.split(__file__)[0], 'output')
 
-names = [("Electron_neutrino.collision_integrals.txt", "Electron_neutrino.number_and_energy_density.txt", []),
-         ("Muon_neutrino.collision_integrals.txt", "Muon_neutrino.number_and_energy_density.txt", []), 
-         ("Tau_neutrino.collision_integrals.txt", "Tau_neutrino.number_and_energy_density.txt", [])]
+names = [
+    ("Electron_neutrino.collision_integrals.txt", "Electron_neutrino.number_and_energy_density.txt", []),
+    ("Muon_neutrino.collision_integrals.txt", "Muon_neutrino.number_and_energy_density.txt", []),
+    ("Tau_neutrino.collision_integrals.txt", "Tau_neutrino.number_and_energy_density.txt", [])
+]
 
-scale_factor = (np.loadtxt(folder + "/" + "evolution.txt", unpack=True)[2])[:500]
+g = 2
+scale_factor = (np.loadtxt(folder + "/" + "evolution.txt", unpack=True)[2])[:5000]
 
 for namecol, nameden, integral in names:
 
@@ -24,24 +24,26 @@ for namecol, nameden, integral in names:
     number_den = (densities[0])[:5000]
 
     # \frac{1}{2\pi^2 a^3}\int{dy y^2 \tilde{I}_{code}} / (na^3)
-    for N, integrand in enumerate(data[1:5001]): # row 1 is momenta
-        temp = simps(momenta**2 * integrand, momenta)/(2*np.pi**2 * (scale_factor[N])**3)
-        integral.append(temp / (number_den[N] * scale_factor[N]**3))
+    for N, integrand in enumerate(data[1:5001]):  # row 1 is momenta
+        temp = (g / (2 * np.pi**2) * simps(momenta**2 * integrand, momenta)
+                / (number_den[N] * scale_factor[N]**3))
+        integral.append(temp)
 
 a, b, integrals_e = names[0]
 c, d, integrals_mu = names[1]
 e, f, integrals_tau = names[2]
 
 
-plt.plot(scale_factor[:5000] , np.abs(integrals_e), color="red", label="e")
-plt.plot(scale_factor[:5000] , np.abs(integrals_mu), color="blue", label="mu")
-plt.plot(scale_factor[:5000] , np.abs(integrals_tau), color="green", label="tau")
+plt.plot(scale_factor[1:5000], (integrals_e), color="red", label="e")
+plt.plot(scale_factor[1:5000], (integrals_mu), color="blue", label="mu")
+plt.plot(scale_factor[1:5000], (integrals_tau), color="green", label="tau")
 
 plt.xlabel("Scale factor", fontsize=16)
-plt.ylabel(r"$|\ \frac{g}{2\pi^2a^3}\int{dy\ y^2\tilde{I}_{code}} \cdot \frac{1}{na^3}\ |$", fontsize=16)
+plt.ylabel(r"$\frac{d (n a^3)}{dy (n a^3)} =\ \frac{g}{2\pi^2 (na^3)}\int{dy\ y^2 I_{code}}$", fontsize=16)
 plt.title("Elastic processes only")
-plt.semilogy()
+# plt.semilogy()
+# plt.yscale('symlog')
 plt.semilogx()
 plt.legend(loc="upper right")
 plt.show()
-
+input("...")
