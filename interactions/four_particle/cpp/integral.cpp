083f6fe084f585eff30ca03ea6b3b02de883c57d
Andrii Magalich <andrew.magalich@gmail.com>
2018-03-08 15:24:18 +0100

    Use non-singular integrator

--- interactions/four_particle/cpp/integral.cpp
+++ interactions/four_particle/cpp/integral.cpp
@@ -544,8 +544,8 @@ dbl integrand_2nd_integration(
     F.function = &integrand_1st_integration;
 
     gsl_integration_workspace *w = gsl_integration_workspace_alloc(params.subdivisions);
-    status = gsl_integration_qags(&F, min_2, max_2, params.abseps, params.releps, params.subdivisions, w, &result, &error);
-    // status = gsl_integration_qag(&F, g, h, params.abseps, params.releps, params.subdivisions, GSL_INTEG_GAUSS15, w, &result, &error);
+    // status = gsl_integration_qags(&F, min_2, max_2, params.abseps, params.releps, params.subdivisions, w, &result, &error);
+    status = gsl_integration_qag(&F, min_2, max_2, params.abseps, params.releps, params.subdivisions, GSL_INTEG_GAUSS15, w, &result, &error);
     if (status) {
         printf("(p0=%e, p1=%e) 1st integration result: %e ± %e. %i intervals. %s\n", params.p0, p1, result, error, (int) w->size, gsl_strerror(status));
         throw std::runtime_error("Integrator failed to reach required accuracy");
@@ -614,8 +614,7 @@ std::vector<dbl> integration(
         );
 
         dbl releps = 1e-2;
-        //dbl abseps = 1e-10;
-        dbl abseps = std::max(f / stepsize * releps, 1e-15);
+        dbl abseps = f / stepsize * releps;
 
         size_t subdivisions = 100;
         struct integration_params params = {
@@ -638,24 +637,6 @@ std::vector<dbl> integration(
         }
         gsl_integration_workspace_free(w);
         integral[i] += result;
-
-        // params = {
-        //     p0, 0., 0.,
-        //     &reaction, &Ms,
-        //     ai, bi, gi, hi,
-        //     3, releps, abseps,
-        //     subdivisions
-        // };
-        // F.params = &params;
-
-        // w = gsl_integration_workspace_alloc(subdivisions);
-        // status = gsl_integration_qag(&F, ai, bi, abseps, releps, subdivisions, GSL_INTEG_GAUSS15, w, &result, &error);
-        // if (status) {
-        //     printf("2nd integration_f result: %e ± %e. %i intervals. %s\n", result, error, (int) w->size, gsl_strerror(status));
-        //     throw std::runtime_error("Integrator failed to reach required accuracy");
-        // }
-        // gsl_integration_workspace_free(w);
-        // integral[i] += result;
     }
 
     return integral;
