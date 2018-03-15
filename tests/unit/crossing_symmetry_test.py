4cdddd21e72d6b2b01c5a38409aefecc9cdb8f29
Andrii Magalich <andrew.magalich@gmail.com>
2018-03-15 14:26:29 +0100

    Add additional test and explanations

--- tests/unit/crossing_symmetry_test.py
+++ tests/unit/crossing_symmetry_test.py
@@ -26,7 +26,14 @@ def neutrino_scattering_amplitude_test(params, universe):
 
 
 @with_setup_args(setup)
-def three_particle_integral_test(params):
+def three_particle_integral_heavy_test(params):
+    """ If M_N > M_pi, there should be integrals for the reactions:
+
+        N <--> nu_e + pi^0
+        nu_e + pi^0 <--> N
+        pi^0 +nu_e <--> N
+        pi^0 + anti-nu_e <--> anti-N
+    """
 
     photon = Particle(**SMP.photon)
     neutrino_e = Particle(**SMP.leptons.neutrino_e)
@@ -39,17 +46,56 @@ def three_particle_integral_test(params):
     })
 
     interaction = NuI.sterile_hadrons_interactions(
-        thetas = thetas, sterile=sterile,
-        neutrinos = [neutrino_e],
-        leptons = [],
-        mesons = [neutral_pion]
+        thetas=thetas, sterile=sterile,
+        neutrinos=[neutrino_e],
+        leptons=[],
+        mesons=[neutral_pion]
     )
 
     universe = Universe(params=params)
     universe.add_particles([photon, neutrino_e, sterile, neutral_pion])
     universe.interactions += interaction
 
-    params.update(universe.total_energy_density(), universe.total_entropy())
+    assert len(universe.interactions) == 2
+    assert len(universe.interactions[0].integrals) == 2
+    assert len(universe.interactions[1].integrals) == 2
+
+    integral = universe.interactions[0].integrals[0]
+    assert len(integral.Ms) == 1
+    assert isinstance(integral.Ms[0].K, (int, float))
+
+
+@with_setup_args(setup)
+def three_particle_integral_light_test(params):
+    """ If M_N < M_pi, there should be integrals for the reactions:
+
+        N + anti-nu_e <--> pion
+        nu_e + anti-N <--> pion
+        pi^0 <--> anti-nu_e + N
+        pi^0 <--> nu_e + anti-N
+    """
+
+    photon = Particle(**SMP.photon)
+    neutrino_e = Particle(**SMP.leptons.neutrino_e)
+    sterile = Particle(**NuP.dirac_sterile_neutrino(mass=100 * UNITS.MeV))
+    neutral_pion = Particle(**SMP.hadrons.neutral_pion)
+
+    theta = 1e-3
+    thetas = defaultdict(float, {
+        'electron': theta,
+    })
+
+    interaction = NuI.sterile_hadrons_interactions(
+        thetas=thetas, sterile=sterile,
+        neutrinos=[neutrino_e],
+        leptons=[],
+        mesons=[neutral_pion]
+    )
+
+    universe = Universe(params=params)
+    universe.add_particles([photon, neutrino_e, sterile, neutral_pion])
+    universe.interactions += interaction
+
     assert len(universe.interactions) == 2
     assert len(universe.interactions[0].integrals) == 2
     assert len(universe.interactions[1].integrals) == 2
