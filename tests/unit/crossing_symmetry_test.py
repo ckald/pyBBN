0c50479385cf8fb5ad2699f682c358dc65c7adb8
Andrii Magalich <andrew.magalich@gmail.com>
2018-03-29 16:10:36 +0300

    Add a clearer test

--- tests/unit/crossing_symmetry_test.py
+++ tests/unit/crossing_symmetry_test.py
@@ -141,41 +141,63 @@ def four_particle_integrals_check_test(params):
 
     for inter in interaction:
         for integral in inter.integrals:
-            count = Counter(item.specie.name for item in integral.reaction)
-            if count['Electron neutrino'] and count['Electron'] and count['Sterile neutrino (Dirac)']:
-                if integral.reaction[0].specie.name == 'Sterile neutrino (Dirac)':
+            count = Counter(item.specie for item in integral.reaction)
+            if count[neutrino_e] and count[electron] and count[sterile]:
+                if integral.particle == sterile:
                     NC_HNL += 1
-                if integral.reaction[0].specie.name == 'Electron neutrino':
+                if integral.particle == neutrino_e:
                     NC_nu += 1
-                if integral.reaction[0].specie.name == 'Electron':
+                if integral.particle == electron:
                     NC_e += 1
 
-            if count['Muon neutrino'] and count['Electron'] and count['Muon'] and count['Sterile neutrino (Dirac)']:
-                if integral.reaction[0].specie.name == 'Sterile neutrino (Dirac)':
+            if count[neutrino_mu] and count[electron] and count[muon] and count[sterile]:
+                if integral.particle == sterile:
                     CC_HNL += 1
-                if integral.reaction[0].specie.name == 'Muon neutrino':
+                if integral.particle == neutrino_mu:
                     CC_nu += 1
-                if integral.reaction[0].specie.name == 'Muon':
+                if integral.particle == muon:
                     CC_mu += 1
-                if integral.reaction[0].specie.name == 'Electron':
+                if integral.particle == electron:
                     CC_e += 1
 
-    string_1 = "Incorrect amount of collision integrals for "
-    string_2 = " interactions created. Expected: "
-    string_3 = " Obtained: "
+    tpl = (
+        "Incorrect amount of collision integrals for {part} in {int}" +
+        " interactions created. Expected: {exp}. Obtained: {val}"
+    )
 
-    assert NC_HNL == 3, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "HNL", interaction="NC", string2=string_2, expect=3, string3=string_3, obtain=NC_HNL)
-    assert NC_nu == 3, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "nu_e", interaction="NC", string2=string_2, expect=3, string3=string_3, obtain=NC_nu)
-    assert NC_e == 4, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "e", interaction="NC", string2=string_2, expect=4, string3=string_3, obtain=NC_e)
+    assert NC_HNL == 3, tpl.format(part="HNL", int="NC", exp=3, val=NC_HNL)
+    assert NC_nu == 3, tpl.format(part="nu_e", int="NC", exp=3, val=NC_nu)
+    assert NC_e == 4, tpl.format(part="e", int="NC", exp=4, val=NC_e)
 
-    assert CC_HNL == 4, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "HNL", interaction="CC", string2=string_2, expect=4, string3=string_3, obtain=CC_HNL)
-    assert CC_nu == 4, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "nu", interaction="CC", string2=string_2, expect=4, string3=string_3, obtain=CC_nu)
-    assert CC_mu == 4, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "mu", interaction="CC", string2=string_2, expect=4, string3=string_3, obtain=CC_mu)
-    assert CC_e == 4, "{string1}{particle} in {interaction}{string2}{expect}{string3}{obtain}".format(string1 = string_1,
-                        particle = "e", interaction="CC", string2=string_2, expect=4, string3=string_3, obtain=CC_e)
+    assert CC_HNL == 4, tpl.format(part="HNL", int="CC", exp=4, val=CC_HNL)
+    assert CC_nu == 4, tpl.format(part="nu", int="CC", exp=4, val=CC_nu)
+    assert CC_mu == 4, tpl.format(part="mu", int="CC", exp=4, val=CC_mu)
+    assert CC_e == 4, tpl.format(part="e", int="CC", exp=4, val=CC_e)
+
+
+@with_setup_args(setup)
+def neutral_channel_test(params):
+    neutrino = Particle(**SMP.leptons.neutrino_e)
+    lepton = Particle(**SMP.leptons.electron)
+    sterile = Particle(**NuP.dirac_sterile_neutrino(mass=150 * UNITS.MeV))
+
+    theta = 1e-3
+    thetas = defaultdict(float, {
+        'electron': theta,
+    })
+
+    interaction = NuI.sterile_active_to_leptons_NC(
+        theta=thetas[neutrino.flavour],
+        g_L=CONST.g_L,
+        sterile=sterile,
+        active=neutrino,
+        lepton=lepton
+    )
+
+    counter = Counter(integral.particle.name
+                      for integral in interaction.integrals)
+    assert counter[sterile.name] == 3
+    assert counter[neutrino.name] == 3
+    assert counter[lepton.name] == 4
+
+    assert len(interaction.integrals) == 3
